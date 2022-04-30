c985b17cb7b81bb81a1032f8810f7030809caf7e
Talha Bhatti <80376242+talhabhathi@users.noreply.github.com>
2022-04-30 14:28:27 +0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -282,14 +282,25 @@
                                 </fieldset>
                                 <fieldset>
                                     <div class="form-card">
-                                        <div class="row">
+                                        <div class="row image_div">
                                             <div class="col-7">
                                                 <h2 class="fs-title">Image Upload:</h2>
                                             </div>
                                             <div class="col-5">
                                                 <h2 class="steps">Step 3 - 4</h2>
                                             </div>
-                                        </div>   <input name="file" id="license_file" type="file"  class="dropify" data-height="100"  />
+                                            <div class="col-12">
+                                                <button class="btn btn-primary float-end add_image" type="button" id="add_image">Add Image</button>
+                                            </div>
+                                            <div class="col-6 mt-2">
+                                                <div class="row">
+                                                    <div class="col-12">
+                                                        <button type="button" class="btn float-end my-3 visibility_none"><i class="fas fa-trash text-danger"></i></button>
+                                                    </div>
+                                                </div>
+                                                <input name="file[]" id="license_file" type="file" class="dropify" data-height="100"  />
+                                            </div>
+                                        {{-- </div>   <input name="file" id="license_file" type="file"  class="dropify" data-height="100"  /> --}}
                                     </div> <input type="submit" id="finish" class="next action-button" value="Submit" /> <input type="button" name="previous" class="previous action-button-previous" value="Previous" />
                                 </fieldset>
                                 <fieldset>
